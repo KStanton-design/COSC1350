9715fad6cc43c43bbc503dfe8a4aba0bd4e208aa
Kathy Stanton <kathystanton@westernwyoming.edu>
2024-09-18 11:23:04 -0600

    Add files via upload

---
+++ m05/helloWorld.js
@@ -0,0 +1,8 @@
+/*
+Name: Kathy Stanton
+File Name: helloWorld.js
+Date: 9/23/24
+*/
+
+let hello = "Hello World";
+console.log(hello);
---
+++ m05/practiceVariables.js
@@ -0,0 +1,29 @@
+/*Kathy Stanton
+Practice Variables
+9/17/24*/
+
+let hiDrT = "Hi Dr. T";
+let days = "4.5"
+let patience = "0";
+let negative = "-10";
+let mood = "true";
+
+console.log(typeof hiDrT);
+console.log(typeof 4.5);
+console.log(typeof 0);
+console.log(typeof -10);
+console.log(typeof true);
+
+console.log("Hi Dr. T!\nLet's get started!");
+console.log('half of 100 is ${100/2}');
+console.log("con"+"cat"+"e"+"nate");
+console.log(8 * null);
+console.log("5"- 1);
+console.log("5"+ 1);
+console.log("five" * 2);
+console.log(false==true);
+
+
+
+
+
