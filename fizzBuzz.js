46a82c264d37961593d9a0355d702dbd1efb7ed0
Kathy Stanton <kathystanton@westernwyoming.edu>
2024-09-23 11:18:48 -0600

    Add files via upload

---
+++ fizzBuzz.js
@@ -0,0 +1,17 @@
+/*
+Kathy Stanton
+fizzBuzz.js
+9/22/24
+*/
+
+
+for (let number = 1; number <= 100; number++) {
+    if (number % 3 === 0) {
+        console.log ("Fizz");
+    } else if (number % 5 === 0) {
+            console.log ("Buzz");
+     } else {
+        console.log (number);
+    }
+
+}
